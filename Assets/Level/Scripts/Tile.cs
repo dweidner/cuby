d2ae909472ed5642b23e7549e2da4307cf60cde8
Tim Öttinger <email@tim-oetting.de>
2015-01-30 23:50:27 +0100

    hinzugefügte Gizmos für die Logik-Tiles innerhalb des Editors

--- Assets/Level/Scripts/Tile.cs
+++ Assets/Level/Scripts/Tile.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
+[Serializable]
 public class Tile : MonoBehaviour {
 	public bool isPassable = true;
 	public bool isDeadly = false;
 
+	// public GlobalVars.DimensionTypes TargetDimension = GlobalVars.DimensionTypes.Dimension1;
+	[SerializeField]
+	public GameObject TargetDimension;
+
+	public GameObject Dimension(){
+		return transform.parent.parent.gameObject;
+	}
 	// Use this for initialization
 	void Start () {
 	}
---
+++ Assets/Level/Scripts/TileGizmo.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileGizmo : MonoBehaviour {
+	private Color passable = Color.green;
+	private Color notPassable = Color.red;
+	private Color dimension1 = Color.red;
+	private Color dimension2 = Color.green;
+	private Color dimension3 = Color.blue;
+
+	  private Component tileComponent;
+
+	  void Start(){
+	    tileComponent = GetComponent<Tile>();
+		if (GetComponent<Tile> ().TargetDimension != null) {
+			passable = GetComponent<Tile> ().TargetDimension.GetComponent<Dimension> ().GUIColor;
+		}
+	  }
+
+	
+
+  void OnDrawGizmos() {
+    switch (GetComponent<Tile>().isPassable) 
+    {
+      case true:
+		Color dimensionColor = gameObject.GetComponentInParent<Dimension>().GUIColor;
+		dimensionColor.a = .5f;
+		Gizmos.color = dimensionColor;
+		break;
+      case false:
+        Gizmos.color = notPassable;
+        break;
+    }
+    Gizmos.DrawCube(transform.position, new Vector3(1, .05f, 1));
+
+
+
+    GameObject targetDimension = (GameObject) GetComponent<Tile>().TargetDimension;
+	
+	
+	//LinkObject.GetComponent<hingeJoint2D>().hingeJoint2D.connectedBody = playerA.rigidbody2D;
+
+
+	if(targetDimension != null){
+//      Debug.Log(GetComponent<Tile>().TargetDimension);
+      //Debug.Log(targetDimension.GUIColor);
+		Color dimensionColor = targetDimension.GetComponent<Dimension> ().GUIColor;
+		Gizmos.color = dimensionColor;
+    }else{
+      Gizmos.color = Color.clear;
+    }
+
+
+    Gizmos.DrawCube(transform.position, new Vector3(.5f, .1f, .5f));
+  }
+}
