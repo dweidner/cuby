cf304e9f6c8b2b9664f4b754b37b155730ef224e
Tim Öttinger <email@tim-oetting.de>
2015-01-31 00:01:47 +0100

    Sonstiger Shit wo ich jetzt auch nciht weiß was ich dazu sagen soll

---
+++ Assets/Editor/TileInspector.cs
@@ -0,0 +1,40 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEditor;
+//
+//[CustomEditor(typeof(Tile))]
+//public class TileInspector : Editor 
+//{
+//	private int oldIndex = 0;
+//
+//    public override void OnInspectorGUI()
+//    {
+//		EditorGUI.BeginChangeCheck ();
+//		int dimensionId = oldIndex;
+//		Tile tile = (Tile)target;
+//
+//        DrawDefaultInspector();
+//
+//        GameObject[] dimensions = GameObject.FindGameObjectsWithTag("Dimension");
+//        string[] dimensionLabels = new string[6];
+//
+//        for( int i = 0; i < dimensions.Length; ++i ){
+//          dimensionLabels[i] = dimensions[i].name;
+//			if(tile.TargetDimension.name == dimensions[i].name){
+//				dimensionId = i;
+//			}
+//		}
+//
+//		dimensionId = EditorGUILayout.Popup("Zieldimension", oldIndex, dimensionLabels);
+//
+//		if (EditorGUI.EndChangeCheck ()) {
+//			Debug.Log (dimensions[dimensionId].name);
+//			tile.TargetDimension = dimensions[dimensionId];
+//
+//			if(oldIndex != dimensionId){
+//				oldIndex = dimensionId;
+//			}
+//		}
+//        //EditorUtility.SetDirty(target);
+//    }
+//}
--- Assets/Level/Scripts/MetaTile.cs
+++ Assets/Level/Scripts/MetaTile.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 
 public class MetaTile : MonoBehaviour {
-	//public enum DimensionTypes { Dimension1, Dimension2, Dimension3 };
 	public GlobalVars.DimensionTypes dimension = GlobalVars.DimensionTypes.Dimension1;
+	public GlobalVars.DimensionTypes targetDimension = GlobalVars.DimensionTypes.Dimension1;
 	
 	// Use this for initialization
 	void Start () {
+
 	}
 	
 	// Update is called once per frame
